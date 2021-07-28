38826a9a32847b553f5f87cbdd8b7cdf26921e00
Daniel Rocha <daniel1990rocha@gmail.com>
2021-07-28 11:27:55 -0300

    Adiciona faker no seed e atualiza o rails

--- db/migrate/20210430122255_create_heroes.rb
+++ db/migrate/20210430122255_create_heroes.rb
@@ -1,7 +1,7 @@
 class CreateHeroes < ActiveRecord::Migration[6.1]
   def change
     create_table :heroes do |t|
-      t.string :name, null: false
+      t.string :name, null: false # O null: false obriga o usuario a inserir a informação na tabela
 
       t.timestamps
     end
