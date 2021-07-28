8d63021ef4478bc53b18058817bb63d5999e6f4d
Daniel Rocha <daniel1990rocha@gmail.com>
2021-07-28 11:26:58 -0300

    Valida o campo name

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,5 @@
 class Hero < ApplicationRecord
+  validates :name, presence: true # deixa a area 'name' com obrigacao de ser preenchida**
 end
+
+# ** rails c > hero = Hero.new > hero.save > hero.errors.messages => {:name=>["can't be blank"]}
